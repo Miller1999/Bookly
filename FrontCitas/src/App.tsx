5aeafcfcc8ba982415164d1c245d2a201eebe8b3
Miller A <millerjavierarias@gmail.com>
2025-09-25 14:15:41 -0500

    feat: :sparkles: Simple View

--- FrontCitas/src/App.tsx
+++ FrontCitas/src/App.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import api from "./services/api";
+
+interface Appointment {
+	id: number;
+	clientName: string;
+	date: string;
+	status: string;
+}
+
 function App() {
+	const [appointments, setAppointments] = useState<Appointment[]>([]);
+
+	useEffect(() => {
+		api.get("/appointments").then((response) => {
+			setAppointments(response.data);
+			console.log(response.data);
+		});
+	}, []);
+
 	return (
 		<>
 			<h1>Sistema de citas</h1>
+			<ul>
+				{appointments.map((appointment) => (
+					<li key={appointment.id}>
+						{appointment.clientName} - {appointment.date} - {appointment.status}
+					</li>
+				))}
+			</ul>
 		</>
 	);
 }
